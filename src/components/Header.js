da80fab812187e36d661790bae262b3b96fe83ad
kaustubh <kaustubh285@gmail.com>
2020-09-03 16:44:28 +0530

    Favicon update

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ export default function Header() {
   return (
     <Typography component='div' variant='body1'>
       <Box bgcolor='primary.main' color='primary.contrastText' p={2}>
+        <img src='./favicon.ico' style={{ width: 40, marginRight: 20 }}></img>{" "}
         Four Paws pet Medicines
       </Box>
     </Typography>
